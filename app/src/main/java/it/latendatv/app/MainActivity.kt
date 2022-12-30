ffefcae9d8b9c6ae71b708ea70308e3fca00196d
Davide Parpinello <davide.parpinello@studenti.unitn.it>
2022-12-30 13:53:44 +0100

    Initial commit

---
+++ app/src/main/java/it/latendatv/app/MainActivity.kt
@@ -0,0 +1,73 @@
+package it.latendatv.app
+
+import android.app.Activity
+import android.os.Bundle
+import com.google.android.exoplayer2.ExoPlayer
+import com.google.android.exoplayer2.MediaItem
+import com.google.android.exoplayer2.source.hls.HlsMediaSource
+import com.google.android.exoplayer2.upstream.DefaultHttpDataSource
+import it.latendatv.app.databinding.ActivityMainBinding
+
+class MainActivity : Activity() {
+
+    private lateinit var binding: ActivityMainBinding
+    private var exoPlayer: ExoPlayer? = null
+    private var playbackPosition = 0L
+    private var playWhenReady = true
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setView()
+        preparePlayer()
+    }
+
+    private fun setView() {
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+    }
+
+    private fun preparePlayer() {
+        exoPlayer = ExoPlayer.Builder(this).build()
+        exoPlayer?.playWhenReady = true
+        binding.playerView.player = exoPlayer
+        val defaultHttpDataSourceFactory = DefaultHttpDataSource.Factory()
+        val mediaItem = MediaItem.fromUri(URL)
+        val mediaSource =
+            HlsMediaSource.Factory(defaultHttpDataSourceFactory).createMediaSource(mediaItem)
+        exoPlayer?.apply {
+            setMediaSource(mediaSource)
+            seekTo(playbackPosition)
+            playWhenReady = playWhenReady
+            prepare()
+        }
+    }
+
+    private fun releasePlayer() {
+        exoPlayer?.let { player ->
+            playbackPosition = player.currentPosition
+            playWhenReady = player.playWhenReady
+            player.release()
+            exoPlayer = null
+        }
+    }
+
+    override fun onStop() {
+        super.onStop()
+        releasePlayer()
+    }
+
+    override fun onPause() {
+        super.onPause()
+        releasePlayer()
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        releasePlayer()
+    }
+
+    companion object {
+        const val URL = "https://2-fss-1.streamhoster.com/pl_148/amlst:206202-2980948/playlist.m3u8"
+    }
+}
